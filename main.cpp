a2d3ee60bc135b2d24e7c6bb352b93b66ea23920
AMasquelier <38529011+AMasquelier@users.noreply.github.com>
2019-03-18 11:50:28 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,122 @@
+#include <windows.h>
+#include <string>
+#include <iostream>
+#include "timer.h"
+using std::string;
+using namespace std;
+
+string Makecmd(string title)
+{
+	string cmd = "";
+	for (int i = 0; i < title.length(); i++)
+	{
+		if (title[i] == ' ') cmd += '-';
+		else if (title[i] == '-') cmd.pop_back();
+		else if (title[i] == '!' || title[i] == '.' || title[i] == '?' || title[i] == '\'' || title[i] == '?');
+		else cmd += title[i];
+	}
+	return ("start https://genius.com/" +  cmd + "-lyrics");
+}
+
+HWND spotify_hwnd;
+
+BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam)
+{
+	char class_name[80];
+	char title[80];
+	DWORD dwProcessId;
+	GetClassName(hwnd, class_name, sizeof(class_name));
+	GetWindowText(hwnd, title, sizeof(title));
+
+	// get process id based on hwnd
+	GetWindowThreadProcessId(hwnd, &dwProcessId);
+
+	HANDLE proc = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, dwProcessId);
+	
+	if (proc)
+	{
+		LPSTR path = new CHAR[MAX_PATH];//will hold .exe path to be returned
+		DWORD charsCarried = MAX_PATH;
+		BOOL RES = QueryFullProcessImageNameA(proc, NULL, path, &charsCarried);
+		string filename = path;
+		if (filename.find("Spotify.exe") != string::npos)
+		{
+			char wnd_title[256];
+			GetWindowText(hwnd, wnd_title, sizeof(wnd_title));
+			string Title = wnd_title;
+			if (Title != "" && string(class_name) == "Chrome_WidgetWin_0")
+			{
+				spotify_hwnd = hwnd;
+				return FALSE;
+			}
+		}
+		CloseHandle(proc);
+	}
+
+	return TRUE;
+}
+
+HWND FindSpotify()
+{
+	HWND hwnd = GetForegroundWindow(); // get handle of currently active window
+	char wnd_title[256];
+	GetWindowText(hwnd, wnd_title, sizeof(wnd_title));
+
+	EnumWindows(EnumWindowsProc, 0);
+
+	return hwnd;
+}
+
+
+
+DWORD pID;
+
+int main()
+{
+	LPCSTR wname = "Spotify";
+
+	FindSpotify();
+	
+	cout << wname << endl;
+	Clock framerate_timer; framerate_timer.start();
+	double framerate = 60.0;
+	char wnd_title[256];
+	string title, last_title;
+	bool ls = false, s = false;
+	bool open_genius = false;
+	while (true)
+	{
+		if (framerate_timer.duration() >= 1000000.0 / framerate)
+		{
+			// Search Spotify window
+			if (spotify_hwnd == NULL)
+				spotify_hwnd = FindWindowA(0, wname);
+
+			// Event
+			ls = s;
+			if (GetKeyState(VK_F2) & 0x8000)
+				s = true;
+			else
+				s = false;
+
+			framerate_timer.start();
+			if (wnd_title != nullptr) last_title = wnd_title;
+			GetWindowText(spotify_hwnd, wnd_title, sizeof(wnd_title));
+			title = wnd_title;
+			if (s && !ls) // If pushed f2
+			{
+				system(Makecmd(title).c_str());
+				open_genius = false;
+			}
+			if (title != last_title && title != "Spotify")
+			{
+				system("cls");
+				cout << title << endl;
+			}
+			
+		}
+		else Clock::sleep(1000.0 / framerate - framerate_timer.duration() * 0.001);
+	}
+
+	return 0;
+}
